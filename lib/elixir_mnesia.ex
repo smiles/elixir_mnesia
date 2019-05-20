02a2af1ea7afedc8245cb123318dd50113f21b12
smiles <vissa7@gmail.com>
2019-05-20 15:08:46 +0000

    init repo

---
+++ lib/elixir_mnesia.ex
@@ -0,0 +1,18 @@
+defmodule ElixirMnesia do
+  @moduledoc """
+  Documentation for ElixirMnesia.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirMnesia.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_mnesia_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirMnesiaTest do
+  use ExUnit.Case
+  doctest ElixirMnesia
+
+  test "greets the world" do
+    assert ElixirMnesia.hello() == :world
+  end
+end
